4ed7f944c98d8376f311725b8ae18da7f13d6e84
Anthony Infortunio <ainfortunio1@gmail.com>
2022-09-21 21:30:07 -0400

    Restart, add ui
    Restarting from scratch again. This commit adds the ui to the webpage.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,2 @@
 import "./styles/styles.css";
-import { ProjectList, Project } from "./scripts/project.js";
-import { Display } from "./scripts/display.js";
-import { Task } from "./scripts/task.js";
-
-let DEFAULTLIST = new ProjectList();
-let DEFAULTDISPLAY = new Display();
-let DEFAULTPROJECT = new Project("default", true);
-
-DEFAULTDISPLAY.init(DEFAULTLIST, DEFAULTPROJECT);
-DEFAULTLIST.addProject(DEFAULTPROJECT, DEFAULTDISPLAY);
-DEFAULTPROJECT.addTask(new Task("Default Task","Default Description","09/15/2022",0,"Default Notes",0,DEFAULTPROJECT.taskArray.length), DEFAULTDISPLAY);
-
-//feature: needs to be able to switch project names and update task fields
-//after that use css to style the current Project.
-//maybe add something to highlight the current Project when update display is called that would mean calling update display within the switch project function.
-
-//update task indexes should take priority into account. for example if a prority is high it will be placed first on the list
-//bug warning if all projects are deleted no project will be set as the current project, also if currentProject is deleted there is no longer any currentProject which breaks the program.
+import { Task } from "./scripts/todo.js";
---
+++ src/scripts/todo.js
@@ -0,0 +1,7 @@
+let Task = function(taskName,taskDesc,taskDueDate){
+    this.taskName=taskName;
+    this.taskDesc=taskDesc;
+    this.taskDueDate=taskDueDate;
+};
+
+export {Task};
